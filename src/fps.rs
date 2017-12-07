8bbc6360bd1a6717b364d3c52559f5cfb014da94
James Green <jagreen@thoughtworks.com>
2017-12-07 21:50:53 +0000

    use a rolling sum for fps averaging

--- src/fps.rs
+++ src/fps.rs
@@ -19,7 +19,7 @@ impl FpsCounter {
     pub fn tick(&mut self) {
         let sample = self.last_sampled.elapsed().subsec_nanos();
         self.last_sampled = Instant::now();
-        self.samples.record(sample as u32);
+        self.samples.record(sample);
     }
 
     pub fn current(&self) -> u32 {
@@ -32,6 +32,7 @@ impl FpsCounter {
 struct SampleBuffer {
     samples: Vec<u32>,
     current: usize,
+    rolling_sum: u32,
 }
 
 impl SampleBuffer {
@@ -39,19 +40,21 @@ impl SampleBuffer {
         SampleBuffer {
             samples: vec![0; NUM_SAMPLES],
             current: 0,
+            rolling_sum: 0,
         }
     }
 
     fn record(&mut self, sample: u32) {
+        self.rolling_sum -= self.samples[self.current];
+        self.rolling_sum += sample;
         self.samples[self.current] = sample;
         self.current += 1;
         self.current %= self.samples.len();
     }
 
     fn average(&self) -> u32 {
-        //TODO: Better rolling average - look in DSP book
-        let avg_ms_frame = self.samples.iter().sum::<u32>() / self.samples.len() as u32;
-        (1. / (avg_ms_frame as f64 * 1e-9)) as u32
+        let avg = self.rolling_sum / self.samples.len() as u32;
+        (1. / (avg as f64 * 1e-9)) as u32
     }
 
 }
